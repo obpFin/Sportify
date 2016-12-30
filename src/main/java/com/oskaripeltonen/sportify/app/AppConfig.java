12134a0f8e4f412110689a6b9bc792f56d3f5bff
Oskari Peltonen <oskari.peltonen@outlook.com>
2016-12-30 10:39:18 +0200

    spring initialization

---
+++ src/main/java/com/oskaripeltonen/sportify/app/AppConfig.java
@@ -0,0 +1,16 @@
+package com.oskaripeltonen.sportify.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by admin on 30/12/16.
+ */
+
+@EnableAutoConfiguration
+public class AppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class, args);
+    }
+}
