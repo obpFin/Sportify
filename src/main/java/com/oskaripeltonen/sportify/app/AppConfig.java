9a17067cf028a56f4bbb8b9c5790e8a42623ed7b
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-01-03 11:13:14 +0200

    App controller and thymeleaf support

--- src/main/java/com/oskaripeltonen/sportify/app/AppConfig.java
+++ src/main/java/com/oskaripeltonen/sportify/app/AppConfig.java
@@ -2,12 +2,14 @@ package com.oskaripeltonen.sportify.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by admin on 30/12/16.
  */
 
 @EnableAutoConfiguration
+@ComponentScan
 public class AppConfig {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/oskaripeltonen/sportify/app/controller/AppController.java
@@ -0,0 +1,18 @@
+package com.oskaripeltonen.sportify.app.controller;
+
+/**
+ * Created by admin on 03/01/17.
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String homepage() {
+
+
+        return "index";
+    }
+}
