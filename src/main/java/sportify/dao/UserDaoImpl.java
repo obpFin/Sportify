e5c33f0f177ecef71f38aad61a262fe79d927894
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-01-07 14:35:44 +0200

    Test case & UserDaoImpl

--- src/main/java/sportify/dao/UserDaoImpl.java
+++ src/main/java/sportify/dao/UserDaoImpl.java
@@ -1,10 +1,13 @@
 package sportify.dao;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import sportify.model.User;
+
+import javax.annotation.PostConstruct;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
@@ -18,29 +21,54 @@ public class UserDaoImpl implements UserDao{
     @Autowired
     private SessionFactory sessionFactory;
 
+
     @Override
     public User findById(int id) {
-
         Session session = sessionFactory.getCurrentSession();
-        CriteriaBuilder builder = session.getCriteriaBuilder();
-        CriteriaQuery<User> criteria = builder.createQuery(User.class);
-
+        try {
+            User user = (User) session.get(User.class, id);
+            System.out.println(user.getUserId() + " - " + user.getUserName());
+            session.getTransaction().commit();
+            return user;
+        }
+        catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
         return null;
+
     }
 
     @Override
     public void save(User user) {
-
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            session.close();
+        }
+        catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
+
 
     }
 
     @Override
     public void delete(User user) {
+        Session session = sessionFactory.openSession();
+        try {
+            session.beginTransaction();
+            session.delete(user);
+            session.getTransaction().commit();
+            session.close();
+        }
+        catch (HibernateException e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        }
 
     }
 }
